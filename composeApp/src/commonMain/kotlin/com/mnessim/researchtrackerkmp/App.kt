becbc3e835f28f9b186801b7a29cc01d7395e953
Mounir Nessim <mmnessim@gmail.com>
2025-12-21 09:02:35 -0500

    Add dark mode toggle

--- composeApp/src/commonMain/kotlin/com/mnessim/researchtrackerkmp/App.kt
+++ composeApp/src/commonMain/kotlin/com/mnessim/researchtrackerkmp/App.kt
@@ -1,11 +1,16 @@
 package com.mnessim.researchtrackerkmp
 
-import androidx.compose.animation.EnterTransition
-import androidx.compose.animation.ExitTransition
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.InvertColors
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -15,6 +20,9 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -23,6 +31,7 @@ import androidx.navigation.compose.rememberNavController
 import androidx.navigation.toRoute
 import com.mnessim.researchtrackerkmp.presentation.screens.detailsscreen.DetailsScreen
 import com.mnessim.researchtrackerkmp.presentation.screens.homescreen.HomeScreen
+import com.mnessim.researchtrackerkmp.presentation.theme.darkScheme
 import com.mnessim.researchtrackerkmp.presentation.theme.lightScheme
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -33,6 +42,7 @@ fun App() {
     val navController = rememberNavController()
     val navBackStackEntry by navController.currentBackStackEntryAsState()
     val canPop = navBackStackEntry?.destination?.route != Home::class.qualifiedName
+    var colorScheme by remember { mutableStateOf(lightScheme) }
 
     Scaffold(
         topBar = {
@@ -46,40 +56,61 @@ fun App() {
                     }
                 } else {
                     {}
+                },
+                actions = {
+                    IconButton(onClick = {
+                        colorScheme = if (colorScheme == lightScheme) darkScheme else lightScheme
+                    }) {
+                        Icon(
+                            imageVector = Icons.Default.InvertColors,
+                            contentDescription = "Toggle Dark Mode"
+                        )
+                    }
                 }
             )
         }
     ) { innerPadding ->
         MaterialTheme(
-            colorScheme = lightScheme
+            colorScheme = colorScheme
         ) {
-            NavHost(
-                navController = navController,
-                startDestination = Home,
-                enterTransition = { EnterTransition.None },
-                exitTransition = { ExitTransition.None },
-                popEnterTransition = { EnterTransition.None },
-                popExitTransition = { ExitTransition.None }
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(MaterialTheme.colorScheme.primaryContainer)
             ) {
-                composable<Home> {
-                    HomeScreen(
-                        modifier = Modifier.padding(innerPadding).fillMaxSize(),
-                        onNavigate = { id -> navController.navigate(Details(id)) })
-                }
-                composable<Details> { backStackEntry ->
-                    val details: Details = backStackEntry.toRoute<Details>()
-                    DetailsScreen(
-                        modifier = Modifier.padding(innerPadding).fillMaxSize(),
-                        onBack = {
-                            navController.popBackStack()
-                        },
-                        id = details.id
-                    )
-                }
-            }
-        }
-    }
-
+                NavHost(
+                    navController = navController,
+                    startDestination = Home,
+                    enterTransition = { slideInHorizontally { it } + fadeIn() },
+                    exitTransition = { slideOutHorizontally { -it } + fadeOut() },
+                    popEnterTransition = { slideInHorizontally { -it } + fadeIn() },
+                    popExitTransition = { slideOutHorizontally { it } + fadeOut() }
+                ) {
+                    composable<Home> {
+                        HomeScreen(
+                            modifier = Modifier
+                                .padding(innerPadding)
+                                .fillMaxSize()
+                                .background(MaterialTheme.colorScheme.primaryContainer),
+                            onNavigate = { id -> navController.navigate(Details(id)) })
+                    } // composable<Home>
+                    composable<Details> { backStackEntry ->
+                        val details: Details = backStackEntry.toRoute<Details>()
+                        DetailsScreen(
+                            modifier = Modifier
+                                .padding(innerPadding)
+                                .fillMaxSize()
+                                .background(MaterialTheme.colorScheme.primaryContainer),
+                            onBack = {
+                                navController.popBackStack()
+                            },
+                            id = details.id
+                        )
+                    } // composable<Details>
+                } // NavHost
+            } // Box
+        } // MaterialTheme
+    } // Scaffold
 
 }
 
